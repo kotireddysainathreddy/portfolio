b3b6aee99f78234b3fc48ef94febb4df79f3b444
kotireddysainathreddy <ksainathreddy27@gmail.com>
2024-10-23 22:47:53 +0530

    Describe your changes here

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("email").addEventListener("click", function() {
+    window.location.href = "mailto:ksainathreddy27@gmail.com";
+});
+document.getElementById("linkedin").addEventListener("click", function() {
+    window.open("https://www.linkedin.com/in/ksainathreddy", "_blank"); // Opens in new tab
+});
